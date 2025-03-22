f5f81d9aa4950148f8b22de3eb92fa0a9b509ac3
Shubham Banerjee <32201980+ShubhamCoder007@users.noreply.github.com>
2025-03-22 20:04:51 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 from langchain_groq import ChatGroq
 
 def get_model():
-    llm=ChatGroq(groq_api_key="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",model_name="Gemma2-9b-It")
+    llm=ChatGroq(groq_api_key="",model_name="Gemma2-9b-It")
     return llm
